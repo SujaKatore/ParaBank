ce062c889ba945933d2174d1b383bf48a657a259
SujaKatore <suja.katore@gmail.com>
2025-05-01 14:19:59 +0530

    second commit

--- src/test/java/TestCases/TC_002_Verify_Login_With_Valid_Credentials.java
+++ src/test/java/TestCases/TC_002_Verify_Login_With_Valid_Credentials.java
@@ -3,6 +3,6 @@ package TestCases;
 public class TC_002_Verify_Login_With_Valid_Credentials 
 {
  public static void main(String[] args) {
-	System.out.println("TC_002_Verify_Login_With_Valid_Credentials...");
+	System.out.println("TC_002_Verify_Login_With_Valid_Credentials.....");
 }
 } 
