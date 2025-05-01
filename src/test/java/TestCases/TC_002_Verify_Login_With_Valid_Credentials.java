8326c42387ab3765b69d88e72498730b02dceaff
SujaKatore <suja.katore@gmail.com>
2025-05-01 14:13:32 +0530

    TC2

---
+++ src/test/java/TestCases/TC_002_Verify_Login_With_Valid_Credentials.java
@@ -0,0 +1,8 @@
+package TestCases;
+
+public class TC_002_Verify_Login_With_Valid_Credentials 
+{
+ public static void main(String[] args) {
+	System.out.println("TC_002_Verify_Login_With_Valid_Credentials...");
+}
+} 
